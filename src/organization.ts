3362acc3c5ee7b2ce6d283688d058f37491311f8
Mykola Zarichnyuk <mykola.zarichnyuk@gmail.com>
2019-12-01 20:33:21 +0200

    add name field

--- src/organization.ts
+++ src/organization.ts
@@ -1,10 +1,12 @@
 import {ILogger} from "./interfaces";
 
 export interface IOrganization {
-
+    name: string;
 }
 
 export default class Organization implements IOrganization, ILogger {
+    public name: string;
+
     logData() {
         console.log();
     }
