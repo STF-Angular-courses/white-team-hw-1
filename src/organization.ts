716668ac526d853982c783041681a920c8c6a886
valeriyProg <val.fowork@gmail.com>
2019-12-01 21:52:32 +0200

    Fill Person, Organisation .ts

--- src/organization.ts
+++ src/organization.ts
@@ -1,11 +1,34 @@
 import {ILogger} from "./interfaces";
 
 export interface IOrganization {
-
+    id: string;
+    type: string;
+    name: string;
+    address: string;
+    telephone: string;
+    owner: string | IOrganisation | IPerson;
+    departments: IPostDepartment;  //IDepartment??
 }
 
 export default class Organization implements IOrganization, ILogger {
+    constructor(
+        public id: string,
+        public type: string,
+        public name: string,
+        public address: string,
+        public telephone: string,
+        public owner: string | IOrganisation | IPerson,
+        public departments: IPostDepartment = []
+    ) { }
     logData() {
-        console.log();
+        console.log(
+            '\nCompany: ' + this.name +
+            'ID: ' + this.id + '\n' +
+            'Type: ' + this.type + '\n' +
+            'Address: ' + this.address + '\n' +
+            'Tel: ' + this.telephone + '\n' +
+            'Owner: ' + this.owner + '\n' +
+            'Office Count: ' + this.departments.length + '\n'
+        );
     }
 }
--- src/person.ts
+++ src/person.ts
@@ -2,8 +2,36 @@ import {ILogger} from "./interfaces";
 
 export interface IPerson {
     name: string;
+    id: string;
+    age: number;
+    category: string;
+    address: string;
+    telephone: string;
+    discount: number | boolean;
+    items: IPackage[];
 }
 
-export default class Person implements IPerson, ILogger {
-
+export default class Person implements IPerson, IClient, ILogger {
+    constructor (
+        public name: string,
+        public id: string,
+        public age: number,
+        public category: string,
+        public address: string,
+        public telephone: string,
+        public discount: number | boolean,
+        public items: IPackage[]  = []
+    ) {}
+    logData() {
+        console.log(
+            '\nPerson client: ' + this.name +
+            'ID: ' + this.id + '\n' +
+            'Age: ' + this.age + '\n' +
+            'Category: ' + this.category + '\n' +
+            'Discount: ' + this.discount + '\n' +
+            'Address: ' + this.address + '\n' +
+            'Tel: ' + this.telephone + '\n' +
+            'Packages Count: ' + this.items.length + '\n'
+        );
+    }
 }
