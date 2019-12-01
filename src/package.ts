dbdd25efb6471b738d938b97c76624747956c069
Kolia913 <mynew1mail470@gmail.com>
2019-12-01 21:32:19 +0200

    Added fields height, width, weight, price, type. Added constructor, method logData.

--- src/package.ts
+++ src/package.ts
@@ -7,8 +7,31 @@ export interface IPackage {
     client: IOrganization | IPerson;
     startingPoint: IPostDepartment;
     destinationPoint : IPostDepartment;
+    height : string;
+    width : string;
+    weight : string;
+    price : string;
+    type : string;
 }
 
 export default class Package implements IPackage, ILogger {
-
+    constructor(public client: IOrganization | IPerson,
+                public startingPoint: IPostDepartment,
+                public destinationPoint : IPostDepartment,
+                public type : string,
+                public price : string,
+                public height : string,
+                public width : string,
+                public weight : string) { }
+    logData() {
+        console.log(`------ Package ------
+        Client: ${this.client}
+        Starting point: ${this.startingPoint}
+        Destination point: ${this.destinationPoint}
+        Type: ${this.type}
+        Price: ${this.price}
+        Height: ${this.height}
+        Width: ${this.width}
+        Wight: ${this.weight}`);
+    }
 }
